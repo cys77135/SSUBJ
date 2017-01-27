32c8ea2b40431d1356831c07c6fa6f0b1f0a24ef
cys77135 <cys77135@ssu.ac.kr>
2017-01-27 23:33:44 +0900

    [숭실 밥집] 검색 창 만들기

--- app/src/main/java/com/example/ssubj/MainActivity.java
+++ app/src/main/java/com/example/ssubj/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.ssubj;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity
 {
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private EditText editSearch;
+    private Toolbar toolbar;
+    private ImageButton btnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,9 +25,15 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnSearch = (ImageButton) findViewById(R.id.btn_search);
+        editSearch = (EditText) findViewById(R.id.edit_search);
+
         // Adding Toolbar to the activity
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        editSearch.setVisibility(View.INVISIBLE);
+        toolbar.setVisibility(View.VISIBLE);
 
         // Initializing the TabLayout
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
@@ -42,6 +56,8 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onTabSelected(TabLayout.Tab tab)
             {
+                editSearch.setVisibility(View.INVISIBLE);
+                toolbar.setVisibility(View.VISIBLE);
                 viewPager.setCurrentItem(tab.getPosition());
             }
 
@@ -53,6 +69,19 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onTabReselected(TabLayout.Tab tab)
             {
+            }
+        });
+
+        btnSearch.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                toolbar.setVisibility(View.INVISIBLE);
+                editSearch.setVisibility(View.VISIBLE);
+                editSearch.requestFocus();
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
             }
         });
     }
