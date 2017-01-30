15afce729f09b9d6a419f28926f51ff49284dbb2
cys77135 <cys77135@ssu.ac.kr>
2017-01-30 19:35:38 +0900

    [숭실 밥집] 파일 입출력 기본

--- app/src/main/java/com/example/ssubj/TabFragment3.java
+++ app/src/main/java/com/example/ssubj/TabFragment3.java
@@ -12,6 +12,12 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Spinner;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class TabFragment3 extends Fragment
 {
     private Spinner locationSpinner;
@@ -19,14 +25,16 @@ public class TabFragment3 extends Fragment
     private ListView locationListView;
     private LinearLayout location, locationListSelect;
     private Button locationBtnList, locationBtnSelect, locationBtnBack;
-    String[] LIST_LOCATION = {"LLIST1", "LLIST2", "LLIST3", "LLIST4", "LLIST5", "LLIST6", "LLIST7", "LLIST8", "LLIST9", "LLIST10"};
+    ArrayList<String> listLocation = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.tab_fragment_3, null);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, LIST_LOCATION);
+        loadTxt();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, listLocation);
 
         location = (LinearLayout) view.findViewById(R.id.location);
         locationListSelect = (LinearLayout) view.findViewById(R.id.location_list_select);
@@ -61,8 +69,11 @@ public class TabFragment3 extends Fragment
             {
                 if (v.getId() == R.id.location_list)
                 {
-                    locationListSelect.setVisibility(View.INVISIBLE);
-                    location.setVisibility(View.VISIBLE);
+                    if (!locationSelected.equals("위치 선택"))
+                    {
+                        locationListSelect.setVisibility(View.INVISIBLE);
+                        location.setVisibility(View.VISIBLE);
+                    }
                 }
                 else if (v.getId() == R.id.location_select)
                 {
@@ -88,5 +99,32 @@ public class TabFragment3 extends Fragment
         locationBtnBack.setOnClickListener(locationListener);
 
         return view;
+    }
+
+    public void loadTxt()
+    {
+        InputStream inputData = getResources().openRawResource(R.raw.ssubjalist);
+
+        try
+        {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputData, "EUC_KR"));
+            while (true)
+            {
+                String string = bufferedReader.readLine();
+
+                if (string != null)
+                {
+                    listLocation.add(string);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
